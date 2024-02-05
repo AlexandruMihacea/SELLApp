4cb4df9f6ac4a9dae296f6a6fee324407510a378
Mihacea Alexandru <74827403+AlexandruMihacea@users.noreply.github.com>
2024-02-05 16:00:51 +0200

    login register page and main page done

---
+++ frontend/src/views/ListaAnunturi.vue
@@ -0,0 +1,83 @@
+<script>
+    import { computed, watchEffect } from 'vue';
+    import { useStore } from 'vuex';
+
+    export default {
+    setup() {
+        const store = useStore();
+        const produse = computed(() => store.getters.getProduse);
+
+        watchEffect(() => {
+            store.dispatch('fetchProduse');
+        });
+
+        return {
+            produse
+        };
+    },
+    };
+</script>
+
+
+<template>
+    <div class="container">
+                <div v-for="(produs, index) in produse" :key="index" class="card-iterator">
+                    <div class="card-container">
+                        <img :src=produs.downloadLink alt="imagine produs" class="image-style"/>
+                        <h2>{{ produs.titlu }}</h2>
+                        <h4>{{ produs.descriere }}</h4>
+                        <h3>{{ produs.pret }} LEI</h3>
+                    </div>
+                </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.card-iterator {
+    width: calc(25% - 20px); 
+    margin-bottom: 20px;
+}
+
+.card-container {
+    background-color: #00BF63; 
+    padding: 20px;
+    border-radius: 8px;
+    text-align: center;
+    margin: 5px;
+}
+
+.image-style {
+    width: 100%;
+    max-height: 200px;
+    border-radius: 8px;
+}
+
+h2, h4, h3 {
+    color: black;
+    margin: 10px 0;
+}
+
+@media screen and (max-width: 1200px) {
+    .card-iterator {
+        width: calc(33.33% - 20px); 
+    }
+}
+
+@media screen and (max-width: 800px) {
+    .card-iterator {
+        width: calc(50% - 20px); 
+    }
+}
+
+@media screen and (max-width: 600px) {
+    .card-iterator {
+        width: calc(100% - 20px); 
+    }
+}
+</style>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="register-container">
+      <h2>Înregistrare</h2>
+      <form @submit.prevent="register">
+        <div class="form-group">
+          <label for="firstName">Prenume:</label>
+          <input type="text" id="firstName" v-model="firstName" placeholder="Introduceți prenumele" required>
+        </div>
+        <div class="form-group">
+          <label for="lastName">Nume:</label>
+          <input type="text" id="lastName" v-model="lastName" placeholder="Introduceți numele" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" placeholder="Introduceți adresa de email" required>
+        </div>
+        <div class="form-group">
+          <label for="location">Locație:</label>
+          <input type="text" id="location" v-model="location" placeholder="Introduceți locația" required>
+        </div>
+        <div class="form-group">
+          <label for="phone">Telefon:</label>
+          <input type="tel" id="phone" v-model="phone" placeholder="Introduceți numărul de telefon" required>
+        </div>
+        <div class="form-group">
+          <label for="profileImage">Imagine de profil:</label>
+          <input type="file" id="profileImage" @change="handleImageUpload">
+        </div>
+        <button type="submit">Înregistrare</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        firstName: '',
+        lastName: '',
+        email: '',
+        location: '',
+        phone: '',
+        profileImage: null
+      };
+    },
+    methods: {
+      register() {
+        // Implementați aici logica de înregistrare
+        console.log('Nume:', this.firstName);
+        console.log('Prenume:', this.lastName);
+        console.log('Email:', this.email);
+        console.log('Locație:', this.location);
+        console.log('Telefon:', this.phone);
+        console.log('Imagine de profil:', this.profileImage);
+      },
+      handleImageUpload(event) {
+        this.profileImage = event.target.files[0];
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .register-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  input[type="text"],
+  input[type="email"],
+  input[type="tel"] {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-sizing: border-box;
+  }
+  
+  button {
+    width: 100%;
+    padding: 10px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
